8bff9c24c90cd682265dad6dc06a0f35c3de0d5a
Shashank Sharma <shashank.sharma98@gmail.com>
2019-09-20 00:51:38 +0530

    Home page Temporary, Team page update, coc html

--- js/main.js
+++ js/main.js
@@ -25,14 +25,196 @@ document.addEventListener('DOMContentLoaded', function () {
     const instances = M.Sidenav.init(elems, {});
 });
 
-// Or with jQuery
 
 $(document).ready(function () {
     $('.sidenav').sidenav();
-    $("#scroll-up-button").on('click', function(event) {
+    $("#scroll-up-button").on('click', function (event) {
         $(window).scrollTop(0);
     });
+    $('.slider').slider();
+    let flag = 0;
+    let flag1 = 0;
+    let flag2 = 0;
+    let flag3 = 0;
+    let flag4 = 0;
+    let flag5 = 0;
+    let flag6 = 0;
+    const front_img_one = $('#front-img-1');
+    const front_img_two = $('#front-img-2');
+    const back_img_one = $('#back-img-1');
+    const back_img_two = $('#back-img-2');
+    const domain_title = $('#domain-title');
+    const domain_card = $('#domain-card');
+    const pin1 = $('#pin1');
+
+    const controller = new ScrollMagic.Controller();
+    const scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 3000})
+        .setPin("#pin1")
+        // .addIndicators({name: "1 (duration: 3000)"}) // add indicators (requires plugin)
+        .addTo(controller)
+        .on("progress", function (e) {
+            let scrollDirection = e.target.controller().info("scrollDirection");
+            let progress = e.progress.toFixed(3);
+            if (progress < 0.050 && scrollDirection === 'FORWARD') {
+                if (flag === 0) {
+                    flag = 1;
+                    M.Toast.dismissAll();
+                    M.toast({html: 'Designing'}, 500);
+                }
+            }
+            if (progress < 0.166 && progress > 0.050 && scrollDirection === 'FORWARD') {
+                if (flag1 === 0) {
+                    flag1 = 1;
+                    temp2();
+                    setTimeout(() => {
+                        M.Toast.dismissAll();
+                        M.toast({html: 'Frontend Development'});
+                        console.log('DESING DOWN');
+                        front_img_one.hide();
+                    }, 500);
+                }
+            }
+            if (progress < 0.200 && scrollDirection === 'REVERSE') {
+                if (flag1 === 1) {
+                    flag1 = 0;
+                    M.Toast.dismissAll();
+                    M.toast({html: 'Designing'});
+                    console.log('DESIGN UP');
+                    front_img_one.show();
+                    temp();
+                }
+            }
+            if (progress > 0.200 && progress < 0.400 && scrollDirection === 'FORWARD') {
+                if (flag2 === 0) {
+                    flag2 = 1;
+                    temp();
+                    setTimeout(() => {
+                        M.Toast.dismissAll();
+                        M.toast({html: 'Mobile Development'});
+                        console.log('FRONTEND DOWN');
+                        back_img_one.hide();
+                    }, 500);
+                }
+            }
+            if (progress > 0.200 && progress < 0.400 && scrollDirection === 'REVERSE') {
+                if (flag2 === 1) {
+                    flag2 = 0;
+                    M.Toast.dismissAll();
+                    M.toast({html: 'Frontend Development'});
+                    console.log('FRONTEND UP');
+                    back_img_one.show();
+                    temp2();
+                }
+            }
+            if (progress > 0.400 && progress < 0.600 && scrollDirection === 'FORWARD') {
+                if (flag3 === 0) {
+                    flag3 = 1;
+                    temp2();
+                    setTimeout(() => {
+                        M.Toast.dismissAll();
+                        M.toast({html: 'Backend Development'});
+                        console.log('ANDROID DOWN');
+                        front_img_two.hide();
+                    }, 500);
+                }
+            }
+            if (progress > 0.400 && progress < 0.600 && scrollDirection === 'REVERSE') {
+                if (flag3 === 1) {
+                    flag3 = 0;
+                    M.Toast.dismissAll();
+                    M.toast({html: 'Mobile Development'});
+                    console.log('ANDROID UP');
+                    front_img_two.show();
+                    temp();
+                }
+            }
+            if (progress > 0.600 && progress < 0.800 && scrollDirection === 'FORWARD') {
+                if (flag4 === 0) {
+                    flag4 = 1;
+                    temp();
+                    setTimeout(() => {
+                        M.Toast.dismissAll();
+                        M.toast({html: 'Cloud Development'});
+                        console.log('BACKEND DOWN');
+                        back_img_two.hide();
+                    }, 500);
+                }
+
+            }
+            if (progress > 0.600 && progress < 0.800 && scrollDirection === 'REVERSE') {
+                if (flag4 === 1) {
+                    flag4 = 0;
+                    M.Toast.dismissAll();
+                    M.toast({html: 'Backend Development'});
+                    console.log('BACKEND UP');
+                    back_img_two.show();
+                    temp2();
+                }
+            }
+            if (progress > 0.800 && progress < 1.000 && scrollDirection === 'FORWARD') {
+                if (flag5 === 0) {
+                    flag5 = 1;
+                    M.Toast.dismissAll();
+                    M.toast({html: 'Machine Learning / AI'});
+                    temp2();
+                }
+            }
+            if (progress > 0.800 && progress < 1.000 && scrollDirection === 'REVERSE') {
+                if (flag5 === 1) {
+                    flag5 = 0;
+                    M.Toast.dismissAll();
+                    M.toast({html: 'Cloud Development'});
+                    temp();
+                }
+            }
+        }).on("start end", function (e) {
+            if (e.type === 'end') {
+                front_img_one.hide();
+                front_img_two.hide();
+                back_img_one.hide();
+                back_img_two.hide();
+            }
+        });
+
+    // var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 200})
+    //     .addTo(controller)
+    //     .addIndicators({name: "Trigger Action"}) // add indicators (requires plugin)
+    //     .on("update", function (e) {
+    //         $("#scrollDirection").text(e.target.controller().info("scrollDirection"));
+    //     })
+    //     .on("enter leave", function (e) {
+    //         console.log(e.type);
+    //         temp();
+    //     })
+    //     .on("start end", function (e) {
+    //         $("#lastHit").text(e.type == "start" ? "top" : "bottom");
+    //     })
+    //     .on("progress", function (e) {
+    //         $("#progress").text(e.progress.toFixed(3));
+    //     });
 });
+
+function temp() {
+    console.log('temp');
+    TweenMax.to("#mask", 0.5, {
+        attr: {
+            x: "0%"
+        },
+        repeat: 0,
+        repeatDelay: 0,
+        ease: Power2.easeInOut
+    });
+}
+
+function temp2() {
+    console.log('temp2');
+    TweenMax.to("#mask", 0.5, {
+        attr: {
+            x: "100%"
+        },
+        ease: Power2.easeInOut
+    });
+}
 
 window.onscroll = function () {
     scrollFunction()
